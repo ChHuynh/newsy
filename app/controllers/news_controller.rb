1692f06b4f5328f1af53553ad72317fc6b206e71
ravibkjoshi <ravibkjoshi@gmail.com>
2017-03-04 11:38:41 -0800

    Add Final touches

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 get '/news' do
   @ign_api = HTTParty.get("https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=#{ENV['APIKEY']}")
-
+  
   @techcrunch_api = HTTParty.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=#{ENV['APIKEY']}")
 
   @hackernews_api = HTTParty.get("https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=#{ENV['APIKEY']}")
